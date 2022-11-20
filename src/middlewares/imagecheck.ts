be20d799b8b5aa36ec63247d749fe82523848b91
Marina Ghattas <95654378+marinasamirghattas@users.noreply.github.com>
2022-11-20 18:52:17 +0200

    ..

--- src/middlewares/imagecheck.ts
+++ src/middlewares/imagecheck.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import path from 'path';
 import fs from 'fs';
 
-export function isImageExist(req: Request, res: Response, next: NextFunction) {
+export function isImageExist(req: Request, res: Response, next: NextFunction):void {
   const baseImagePath: string = path.join(
     __dirname,
     `../../assets/fullsize/${req.params.imageName}.jpg`
